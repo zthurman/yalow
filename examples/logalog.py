dfdb7eb337afe4cc89c0cb4683269be6b8276821
Wonkybadonk <zrt81491@gmail.com>
2020-03-08 20:16:26 -0600

    Closes #1

--- examples/logalog.py
+++ examples/logalog.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from yalow import Yalow
 from examples.example_package import super_mega_ukulele
 
-I_AM_GROOT = Path(__file__).parent.parent
 
-logger = Yalow(root_path=I_AM_GROOT, project='logalog').logger
+if __name__ == '__main__':
+    I_AM_GROOT = Path(__file__).parent.parent
 
-logger.info(f'IT\'S ALIVE!!!!!')
-super_mega_ukulele()
+    logger = Yalow(root_path=I_AM_GROOT, project_name='logalog').logger
+
+    logger.info(f'IT\'S ALIVE!!!!!')
+    super_mega_ukulele()
--- yalow/__init__.py
+++ yalow/__init__.py
@@ -13,17 +13,19 @@ class Yalow:
     ----------
     root_path : pathlib.Path
         Root path of project.
-    project : str
-        Name of project.
+    project_name : str
+        Name of project (The default is 'yalow', I know, very imaginative).
+    log_dir_name : str
+        Name of the log directory created in project root (The default is 'log', because as we've already established
+        my imagination is a veritable cornucopia of originality).
     level : logging.Logger.level
-        Default logging level for events added to log.
+        Default logging level for events added to log (The default is logging.DEBUG).
     file_write_mode : str
-        Mode for writing log file, 'w' for overwrite, 'a' for append.
-        Default behavior is overwrite.
+        Mode for writing log file, 'w' for overwrite, 'a' for append (The default behavior is 'w' for overwrite).
 
     Attributes
     ----------
-    log_filename : pathlib.Path
+    log_filepath : pathlib.Path
         Path for generated log file for project.
     logger : logging.Logger
         Project shared logger object.
@@ -31,15 +33,16 @@ class Yalow:
         Responsible for writing the project log to file.
     formatter : logging.Formatter
         Responsible for formatting project log file output.
+
     """
-    def __init__(self, root_path, project='Yalow', level=logging.DEBUG, file_write_mode='w'):
-        if not os.path.exists(root_path / 'log'):
-            os.makedirs(root_path / 'log')
-        self.log_filepath = root_path/f'log/{project}.log'
-        self.logger = logging.getLogger(project)
+    def __init__(self, root_path, project_name='yalow', log_dir_name='log', level=logging.DEBUG, file_write_mode='w'):
+        if not os.path.exists(root_path / log_dir_name):
+            os.makedirs(root_path / log_dir_name)
+        self.log_filepath = root_path/f'log/{project_name}.log'
+        self.logger = logging.getLogger(project_name)
         self.handler = logging.FileHandler(root_path/self.log_filepath, mode=file_write_mode)
         self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
         self.handler.setFormatter(self.formatter)
         self.logger.addHandler(self.handler)
         self.logger.setLevel(level)
-        self.logger.info(f'Logging initialized for project: {project}')
+        self.logger.info(f'Logging initialized for project: {project_name}')
